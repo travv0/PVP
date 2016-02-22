1e15cfd71f4e0f1ce23a7a7ca57958e40a05727c
Travis <tssund93@gmail.com>
2016-02-22 13:12:14 -0500

    added more logging

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ int main(int argc, char *args[])
 				strcmp(args[1], "--debug") == 0)) {
 		DEBUG = TRUE;
 		clearfile(LOG_FILE);
-		logstr("Debugging enabled.");
+		logstr("Debugging enabled");
 	} else
 		DEBUG = FALSE;
 
@@ -27,25 +27,29 @@ int main(int argc, char *args[])
 	if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) != 0) {
 		throw_err(SDL_INIT_ERR);
 	}
+	logstr("SDL initialized");
 
-	window = SDL_CreateWindow("AcAdvGame", SDL_WINDOWPOS_UNDEFINED,
+	window = SDL_CreateWindow("PVP", SDL_WINDOWPOS_UNDEFINED,
 			SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
 
 	if (window == NULL) {
 		throw_err(SDL_WIND_ERR);
 	}
+	logstr("Main window created");
 
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
 	if (renderer == NULL) {
 		throw_err(SDL_REND_ERR);
 	}
+	logstr("Renderer created");
 
 	screen = SDL_GetWindowSurface(window);
 
 	if (screen == NULL) {
 		throw_err(SDL_SURF_ERR);
 	}
+	logstr("Main surface created");
 
 	if (SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 255, 255, 255)) != 0) {
 		throw_err(SDL_RECT_ERR);
