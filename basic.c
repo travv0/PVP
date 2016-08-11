cfcd077567a31598053b8ce90856768f061b3a82
Travis <tssund93@gmail.com>
2016-08-11 15:37:50 -0400

    added scoring, made it so ball detects collision with paddle before it's inside of it

---
+++ basic.c
@@ -0,0 +1,4 @@
+#include "basic.h"
+
+int PLAYER_SCORE = 0;
+int ENEMY_SCORE = 0;
--- basic.h
+++ basic.h
@@ -1,2 +1,5 @@
 #define BALL_START_VEL	8
 #define BALL_COOLDOWN	60
+
+extern int PLAYER_SCORE;
+extern int ENEMY_SCORE;
