2295fd80a17b6636531287a0c0fd5d48bd98c4cb
Travis <tssund93@gmail.com>
2017-10-05 16:17:17 +0900

    making PVPengine folder submodule

--- PVPengine/copy_contents_to_parent/basic.h
+++
@@ -1,6 +0,0 @@
-/* the frame rate we're trying to hit.
- * make sure this is a double so we don't have to worry about accidentally
- * demoting to int when using it. */
-#define TARGET_FRAME_RATE	144.0
-#define WIN_WIDTH		640
-#define WIN_HEIGHT		480
